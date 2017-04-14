a994c84011cf917144e5d19b98b71646c55248b9
Antonio Atta <atta@uneb.br>
2017-04-14 19:25:34 -0300

    Sugestão de revisão de QuestãoATTA.c
    Pedro,
    Sua solução está bem estruturada e funcional. Tenho duas observações apenas que faço já alterando no seu código para que você as veja no local em que foram feitas:
    1. Não se deve retornar arrays alocados localmente em uma função (foi o que você fez ao retornar "certo" por meio de "r" na função "conserta". Isto porque o array não é retornado por inteiro, apenas um ponteiro para o seu início e conceitualmente ele deixa de existir quando a função termina. Apesar de funcionar na maioria dos compiladores (porque o array não é de fato apagado naquele momento), isto pode não ser sempre válido. Nesse caso, o melhor seria já alterar o array "nome" dentro da própria função como sugerido neste update.
    2. O strcmp da função "main" para interromper o laço ao digitar "FIM" está incorreto pois não leva em consideração a existência do caracter <Enter> no final da cadeia inserido pela função fgets. Sugiro usar strncmp conforme código acima.

--- Questão ATTA.c
+++ Questão ATTA.c
@@ -3,87 +3,73 @@
 #include <string.h>
 #define TAM 5
 
-char* conserta(char *nome){
+void* conserta(char *nome){
 char *r, certo[100];
-if (strstr(nome, "Excelentíssimo Senhor")){
-    strcpy(certo, "Exmo. ");
-    strcat(certo, conserta(strstr(nome, "Senhor")));
-    r=certo;
-    return r;
+if (strstr(nome, "Excelentissimo Senhor")){
+    strcpy(certo, "Exmo. Sr.");
+    strcat(certo, nome+strlen("Excelentissimo Senhor"));
+    strcpy(nome, certo);
 }
-if (strstr(nome, "Excelentíssima Senhora")){
-    strcpy(certo, "Exma. ");
-    strcat(certo, conserta(strstr(nome, "Senhora")));
-    r=certo;
-    return r;
+if (strstr(nome, "Excelentissima Senhora")){
+    strcpy(certo, "Exma. Sra.");
+    strcat(certo, nome+strlen("Excelentissima Senhora"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Ilustrissima Senhora")){
-    strcpy(certo, "Ilma. ");
-    strcat(certo, conserta(strstr(nome, "Senhora")));
-    r=certo;
-    return r;
+    strcpy(certo, "Ilma. Sra.");
+    strcat(certo, nome+strlen("Ilustrissima Senhora"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Ilustrissimo Senhor")){
-    strcpy(certo, "Ilmo. ");
-    strcat(certo, conserta(strstr(nome, "Senhor")));
-    r=certo;
-    return r;
+    strcpy(certo, "Ilmo. Sr.");
+    strcat(certo, nome+strlen("Ilustrissimo Senhor"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Vossa Excelencia")){
-    strcpy(certo, "V.");
-    strcat(certo, conserta(strstr(nome, "Excelencia")));
-    r=certo;
-    return r;
+    strcpy(certo, "V. Ex.a");
+    strcat(certo, nome+strlen("Vossa Excelencia"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Vossa Magnificencia")){
-    strcpy(certo, "V.");
-    strcat(certo, conserta(strstr(nome, "Magnificencia")));
-    r=certo;
-    return r;
+    strcpy(certo, "V. Mag.a");
+    strcat(certo, nome+strlen("Vossa Magnificencia"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Vossa Senhoria")){
-    strcpy(certo, "V.");
-    strcat(certo, conserta(strstr(nome, "Senhoria")));
-    r=certo;
-    return r;
+    strcpy(certo, "V.S.a");
+    strcat(certo, nome+strlen("Vossa Senhoria"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Senhoria")){
     strcpy(certo, "S.a");
-    strcat(certo, strchr(nome, ' '));
-    r=certo;
-    return r;
+    strcat(certo, nome+strlen("Senhoria"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Senhora")){
     strcpy(certo, "Sra.");
-    strcat(certo, strchr(nome, ' '));
-    r=certo;
-    return r;
+    strcat(certo, nome+strlen("Senhora"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Senhor")){
     strcpy(certo, "Sr.");
-    strcat(certo, strchr(nome, ' '));
-    r=certo;
-    return r;
+    strcat(certo, nome+strlen("Senhor"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Dona")){
     strcpy(certo, "D.");
-    strcat(certo, strchr(nome, ' '));
-    r=certo;
-    return r;
+    strcat(certo, nome+strlen("Dona"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Excelencia")){
     strcpy(certo, "Ex.a");
-    strcat(certo, strchr(nome, ' '));
-    r=certo;
-    return r;
+    strcat(certo, nome+strlen("Excelencia"));
+    strcpy(nome, certo);
 }
 if (strstr(nome, "Magnificencia")){
     strcpy(certo, "Mag.a");
-    strcat(certo, strchr(nome, ' '));
-    r=certo;
-    return r;
+    strcat(certo, nome+strlen("Magnificencia"));
+    strcpy(nome, certo);
 }
-return nome;
 }
 
 int main(){
@@ -91,8 +77,9 @@ char *t, entrada[100], Nomes[TAM][100];
 int i, cont=0;
 for (i=0; i<TAM; i++){
     fgets(entrada, 99, stdin);
-    if (!(strcmp(entrada, "FIM"))) break;
-    strcpy(Nomes[i], conserta(entrada));
+    if (!(strncmp(entrada, "FIM", 3))) break;
+    strcpy(Nomes[i], entrada);
+    conserta(Nomes[i]);
     cont++;
     }
 for(i=0; i<cont; i++){
